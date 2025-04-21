568912bf5ab69aa8db2c673cc1319caa7c3b6b39
KotrixB <ricardo.bravo@energyatech.com>
2025-04-20 21:55:15 -0500

    fragment and recycle view v1.1.0

--- TeleHotel/app/src/main/java/com/example/telehotel/features/cliente/adapters/FullscreenPhotoAdapter.java
+++ TeleHotel/app/src/main/java/com/example/telehotel/features/cliente/adapters/FullscreenPhotoAdapter.java
@@ -1,12 +1,10 @@
-package com.example.telehotel.features.cliente;
+package com.example.telehotel.features.cliente.adapters;
 
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;

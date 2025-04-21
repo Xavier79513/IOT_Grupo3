18e2156626d3aab08ac6cff021b1708238d5b73f
Sebastian <a20206438@pucp.edu.pe>
2025-04-21 02:13:23 -0500

    vistas mejoradas

--- TeleHotel/app/src/main/java/com/example/telehotel/features/cliente/PagoExitosoActivity.java
+++ TeleHotel/app/src/main/java/com/example/telehotel/features/cliente/PagoExitosoActivity.java
@@ -10,9 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.telehotel.R;
 
 public class PagoExitosoActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cliente_activity_pagoexitoso);
+
+        Button btnVerReservas = findViewById(R.id.btnVerReservas);
+        btnVerReservas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PagoExitosoActivity.this, ClienteMainActivity.class);
+                startActivity(intent);
+                finish(); // Opcional, si no quieres que regrese a esta pantalla con "Back"
+            }
+        });
     }
 }
